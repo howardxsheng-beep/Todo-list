6cd6d33122137fe9e8b8a090712179fc6088d1e8
ho <howardxsheng@gmail.com>
2026-01-24 23:19:33 +0800

    feat: add axios interceptor to include auth token

--- src/api/client.js
+++ src/api/client.js
@@ -1,9 +1,22 @@
 import axios from 'axios';
-const BASE_URL = "https://todolist-api.hexschool.io"
+import Cookies from "js-cookie";
+
+const BASE_URL = "https://todolist-api.hexschool.io";
 export const api = axios.create({
     baseURL: BASE_URL,
     headers:{'Content-Type':"application/json"},
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("token");
+    if (token) {
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export const getApiError = (err)=>{
     return err?.response?.data?.message|| err?.message || "發生錯誤";
